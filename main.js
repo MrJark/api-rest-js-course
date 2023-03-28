6dd09f3f301e373ff8e639ecde392b08a31f61ce
MrJark <mrjark7@gmail.com>
2023-03-28 07:48:16 +0200

    've added the key api

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
-const url = 'https://api.thecatapi.com/v1/images/search';
+const api_url = 'https://api.thecatapi.com/v1/images/search?api_key=live_bKskKbQpYZtsR4DBNUTzV9XJo2tqNGK0iUdj8wWh5am9z504w6QA6I4mBn1Z92om';
 
-// fetch(url)
+// fetch(api_url)
 //     .then(resp => resp.json())
 //     .then(data => {
 //         const img = document.querySelector('img');
@@ -10,7 +10,7 @@ const url = 'https://api.thecatapi.com/v1/images/search';
 
 // const btn = document.querySelector('.reload-btn');
 // btn.onclick = function () {
-//     fetch (url)
+//     fetch (api_url)
 //         .then (response => response.json())
 //         .then(data => {
 //             const cat = document.querySelector('img');
@@ -20,12 +20,24 @@ const url = 'https://api.thecatapi.com/v1/images/search';
 
 //Usando async y await
 
-async function myCat () {
-    const respon = await fetch(url);
-    const data = await respon.json();
+// async function myCat () {
+//     const respon = await fetch(api_url);
+//     const data = await respon.json();
+//     const img = document.querySelector('img');
+//     img.src = data[0].url;
+// }
+
+// const theButton = document.querySelector('button');
+// theButton.onclick = myCat;
+
+//----Otra manera----
+
+async function reload() {
+    const res = await fetch(api_url);
+    const data = await res.json();
+
     const img = document.querySelector('img');
     img.src = data[0].url;
 }
 
-const theButton = document.querySelector('button');
-theButton.onclick = myCat;
+reload();
