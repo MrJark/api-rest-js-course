a90cf8561f40f771908344f238bc22fc477e2086
MrJark <mrjark7@gmail.com>
2023-03-28 06:53:38 +0200

    've used async and await to call the API

--- main.js
+++ main.js
@@ -1,13 +1,31 @@
 
 const url = 'https://api.thecatapi.com/v1/images/search';
 
-const btn = document.querySelector('.reload-btn');
-btn.onclick = function () {
-    fetch (url)
-        .then (response => response.json())
-        .then(data => {
-            const cat = document.querySelector('img');
-            cat.src = data[0].url;
-        });
-};
+// fetch(url)
+//     .then(resp => resp.json())
+//     .then(data => {
+//         const img = document.querySelector('img');
+//         img.src = data[0].url;
+//     })
 
+// const btn = document.querySelector('.reload-btn');
+// btn.onclick = function () {
+//     fetch (url)
+//         .then (response => response.json())
+//         .then(data => {
+//             const cat = document.querySelector('img');
+//             cat.src = data[0].url;
+//         });
+// };
+
+//Usando async y await
+
+async function myCat () {
+    const respon = await fetch(url);
+    const data = await respon.json();
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+}
+
+const theButton = document.querySelector('button');
+theButton.onclick = myCat;
