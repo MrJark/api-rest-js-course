6ad8e5f616a2b5e6fa0a39a0e97a9cde86baf180
MrJark <mrjark7@gmail.com>
2023-03-29 07:39:58 +0200

    'm trying to solve the 400 errors

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 
+//Url general
 const api_url = 'https://api.thecatapi.com/v1/images/search?api_key=live_bKskKbQpYZtsR4DBNUTzV9XJo2tqNGK0iUdj8wWh5am9z504w6QA6I4mBn1Z92om';
+//url para guardar los michis en favs
+const api_url_favourites = 'https://api.thecatapi.com/v1/favourites?api_key=live_bKskKbQpYZtsR4DBNUTzV9XJo2tqNGK0iUdj8wWh5am9z504w6QA6I4mBn1Z92om';
+
+const spanError = document.getElementById('error');
 
 // fetch(api_url)
 //     .then(resp => resp.json())
@@ -32,12 +37,42 @@ const api_url = 'https://api.thecatapi.com/v1/images/search?api_key=live_bKskKbQ
 
 //----Otra manera----
 
-async function reload() {
+async function loadRandomMichis() {
     const res = await fetch(api_url);
     const data = await res.json();
 
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    if (res.status !== 200){
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    }else {
+        const img = document.getElementById('random-img');
+        img.src = data[0].url;
+    }
 }
 
-reload();
+//para guardar michis en fav
+
+async function loadFavouritesMichis() {
+    const res = await fetch(api_url_favourites);
+    const data = await res.json();
+    console.log(data);
+
+    if(res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    }
+}
+
+async function saveFavMichis() {
+    const res = await fetch (api_url_favourites, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            image_id: 12
+        }),
+    });
+    const data = await res.json();
+}
+
+loadRandomMichis();
+loadFavouritesMichis();
