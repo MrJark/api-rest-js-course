6cbf238ea2ff4f017a39f75a39039ba10f823c84
Chema Ferrández <mrjark@MrJark-MacMini.local>
2023-04-21 11:14:43 +0200

    finished the trending preview section

--- movieAPICourse/src/main.js
+++ movieAPICourse/src/main.js
@@ -1,10 +1,11 @@
 
 const urlAPI = 'https://api.themoviedb.org/3/';
-const url_poster_w300 = '';
+const url_poster_w300 = 'https://image.tmdb.org/t/p/w300/';
 
 async function getTrendingMoviesPreview () {
     const res = await fetch (urlAPI + 'trending/movie/day' + API_KEY_all);
     const data = await res.json();
+    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
     const movies = data.results;
     console.log(data, movies);
@@ -15,8 +16,13 @@ async function getTrendingMoviesPreview () {
         const movieImg = document.createElement('img'); // = que para el div pero para la imagen
         movieImg.classList.add('movie-img'); //le añadimos la clase para que coja el css
         movieImg.setAttribute('alt', movie.title); // ('atributo', 'valor del atibuto') -> para colocarle el atributo del alt. Y para saber cuál es, tienes que irte a la consola y ver el título de la película y decirle que sea ese el alt
-        // movieImg.setAttribute('src', )
+        movieImg.setAttribute('src', url_poster_w300 + movie.poster_path); // para llamar a la url donde esté el poster que mide width=300 y a la propiedad de movie poster_path
+        
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer); //permite traer al contenedor de películas 
+
     });
+
 }
 
 getTrendingMoviesPreview();
